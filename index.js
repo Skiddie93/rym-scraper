1b31dd3bc9ab64f0792f8c852fc2a42db7037ed4
Skiddie <domenp93@gmail.com>
2024-03-06 19:02:00 +0100

    uncomment

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import { getYears, getGenres } from "./utils/puppeteer.js"
 import { loopYears, loopGenres } from "./utils/makeYearCharts.js"
 
 async function init(from, to) {
-  //await getYears(from, to);
-  //await loopYears(from, to);
+  await getYears(from, to);
+  await loopYears(from, to);
 
-  //await getGenres();
+  await getGenres();
   await loopGenres();
 }
 
